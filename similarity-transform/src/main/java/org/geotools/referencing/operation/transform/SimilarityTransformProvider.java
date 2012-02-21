5efafefab852adb9562d58311bdab3e8a3ea718c
Oscar Fonts <oscar.fonts@gmail.com>
2012-02-21 10:51:54 +0100

    Improved Javadoc i SimilarityTransformProvider

--- similarity-transform/src/main/java/org/geotools/referencing/operation/transform/SimilarityTransformProvider.java
+++ similarity-transform/src/main/java/org/geotools/referencing/operation/transform/SimilarityTransformProvider.java
@@ -24,18 +24,20 @@ import org.geotools.metadata.iso.citation.Citations;
 import org.geotools.referencing.NamedIdentifier;
 import org.geotools.referencing.operation.MathTransformProvider;
 import org.geotools.referencing.operation.transform.AffineTransform2D;
-import org.opengis.parameter.InvalidParameterNameException;
-import org.opengis.parameter.InvalidParameterValueException;
 import org.opengis.parameter.ParameterDescriptor;
 import org.opengis.parameter.ParameterDescriptorGroup;
 import org.opengis.parameter.ParameterNotFoundException;
 import org.opengis.parameter.ParameterValueGroup;
-import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.MathTransform;
-    
+
 /**
- * @author Oscar Fonts
+ * The provider for the "<cite>Similarity transformation</cite>" (EPSG 9621).
+ * <p>
+ * Note that similarity transform is a special case of an Affine transform 2D.
  *
+ * @source $URL$
+ * @version $Id$
+ * @author Oscar Fonts
  */
 public class SimilarityTransformProvider extends MathTransformProvider {
 
@@ -43,6 +45,9 @@ public class SimilarityTransformProvider extends MathTransformProvider {
 
     // TODO: TRANSLATION_1 and TRANSLATION_2 should be expressed in "target CRS units", not necessarily SI.METER.
     
+    /**
+     * "Ordinate 1 of evaluation point in target CRS" EPSG::8621
+     */
     public static final ParameterDescriptor<Double> TRANSLATION_1 = createDescriptor(
         new NamedIdentifier[] {
             new NamedIdentifier(Citations.EPSG, "Ordinate 1 of evaluation point in target CRS"),
@@ -50,6 +55,9 @@ public class SimilarityTransformProvider extends MathTransformProvider {
         },
         0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, SI.METER); 
 
+    /**
+     * "Ordinate 2 of evaluation point in target CRS" EPSG::8622
+     */
     public static final ParameterDescriptor<Double> TRANSLATION_2 = createDescriptor(
         new NamedIdentifier[] {
             new NamedIdentifier(Citations.EPSG, "Ordinate 2 of evaluation point in target CRS"),
@@ -57,6 +65,9 @@ public class SimilarityTransformProvider extends MathTransformProvider {
         },
         0, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, SI.METER);
     
+    /**
+     * "Scale difference" EPSG::8611
+     */
     public static final ParameterDescriptor<Double> SCALE = createDescriptor(
         new NamedIdentifier[] {
             new NamedIdentifier(Citations.EPSG, "Scale difference"),
@@ -64,6 +75,9 @@ public class SimilarityTransformProvider extends MathTransformProvider {
         },
         1, Double.MIN_NORMAL, Double.POSITIVE_INFINITY, Dimensionless.UNIT);
     
+    /**
+     * "Rotation angle of source coordinate reference system axes" EPSG::8614
+     */
     public static final ParameterDescriptor<Double> ROTATION = createDescriptor(
         new NamedIdentifier[] {
             new NamedIdentifier(Citations.EPSG, "Rotation angle of source coordinate reference system axes"),
@@ -71,6 +85,11 @@ public class SimilarityTransformProvider extends MathTransformProvider {
         },
         0, -180, +180, NonSI.SECOND_ANGLE);
 
+    /**
+     * The parameter group for "Similarity transformation" EPSG::9621.
+     * 
+     * Includes {@link #TRANSLATION_1}, {@link #TRANSLATION_2}, {@link #SCALE}, {@link #ROTATION}.
+     */
     static final ParameterDescriptorGroup PARAMETERS = createDescriptorGroup(
         new NamedIdentifier[] {
             new NamedIdentifier(Citations.EPSG, "Similarity transformation"),
@@ -84,13 +103,35 @@ public class SimilarityTransformProvider extends MathTransformProvider {
         }
     );
     
+    /**
+     * Creates a two-dimensional similarity transform.
+     * 
+     * EPSG defines explicitly this transform as 2D.
+     */
     public SimilarityTransformProvider() {
         super(2, 2, PARAMETERS);
     }
 
+    /**
+     * Constructs an {@link AffineTransform2D} math transform from the specified group of parameter values.
+     * 
+     * The similarity transform is a particular case of Affine Transform 2D where:
+     * 
+     * <blockquote><pre>
+     * m00 = SCALE * cos(ROTATION)
+     * m01 = SCALE * sin(ROTATION)
+     * m02 = TRANSLATION_1
+     * m10 = -m01
+     * m11 = m00
+     * m12 = TRANSLATION_2
+     * </pre></blockquote>
+     *
+     * @param  values The group of parameter values {@link #PARAMETERS}.
+     * @return an {@link AffineTransform2D}.
+     * @throws ParameterNotFoundException if a required parameter was not found.
+     */
     protected MathTransform createMathTransform(ParameterValueGroup values)
-            throws InvalidParameterNameException, ParameterNotFoundException,
-            InvalidParameterValueException, FactoryException {
+            throws ParameterNotFoundException {
         
         // The four parameters
         double t1 = doubleValue(TRANSLATION_1, values);
